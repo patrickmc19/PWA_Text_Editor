ca0ffd670329c538ac5fc9f6ff86cac82bea9413
Patrick McKnight <patrickgmcknight@gmail.com>
2023-05-15 14:11:43 -0700

    attempting to correct indexed db error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,35 +7,30 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { 
-        keyPath: 'id', 
-        autoIncrement: true 
-      });
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('Post to the database');
   const contactDb = await openDB('jate', 1);
   const tx = contactDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ content: content });
+  const request = store.put({ value: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET from the database');
   const contactDb = await openDB('jate', 1);
   const tx = contactDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
